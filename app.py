463d6545eeb919de57a9c79efed2e9ff0f6b9e2a
David Levine <davidlevine00@gmail.com>
2025-01-12 10:43:33 -0500

    Update requirements.txt and app.py

--- app.py
+++ app.py
@@ -2,19 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Route for the home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route for Chapter 1
 @app.route('/chapter1', methods=['GET', 'POST'])
 def chapter1():
     if request.method == 'POST':
-        # Process form data
         answers = request.form
         score = 0
-        # Logic to evaluate answers
         if answers.get('question1') == 'D':
             score += 1
         if answers.get('question2') == 'A':
